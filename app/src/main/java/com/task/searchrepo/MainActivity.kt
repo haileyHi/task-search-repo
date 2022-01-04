1457b52fdf4f063a5a75a30519474cdb4f06f750
haileyHi <yuhiro@naver.com>
2022-01-04 12:59:00 +0900

    :sparkles: [ADD] viewModel 작성 및 연결

--- app/src/main/java/com/task/searchrepo/MainActivity.kt
+++ app/src/main/java/com/task/searchrepo/MainActivity.kt
@@ -2,10 +2,124 @@ package com.task.searchrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.task.searchrepo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var adapter: RepoAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: SearchView
+
+    private lateinit var keyword: String
+    private var page: Int = 1
+    private var isMore: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // searchView 리스너 설정
+        searchView = binding.svKeyword
+        initQueryTextListener()
+
+        // recyclerview 어댑터 연결, 리스너 설정
+        recyclerView = binding.rvSearchResult
+        adapter = RepoAdapter()
+        recyclerView.adapter = adapter
+        initScrollListener()
+
+        // viewModel 필드 observe 일괄 적용하기
+        viewModel.apply {
+            itemLiveData.observe(this@MainActivity, { it ->
+                adapter.updateItems(it)
+            })
+            totalItemCountLiveData.observe(this@MainActivity, { num ->
+                binding.tvResultCount.text = "검색 결과 : $num 개"
+            })
+            loadingLiveData.observe(this@MainActivity, { loading ->
+                binding.progressBar.visibility = if (loading) View.VISIBLE else View.GONE
+            })
+            hasMore.observe(this@MainActivity, {
+                isMore = it
+            })
+        }
+
+    }
+
+    fun loadData(loadingType: Int) {
+        when (loadingType) {
+            0 -> viewModel.fetchSearchInfo() // 새로 가져오기
+            1 -> {
+                viewModel.updateValue(actionType = 1, input = null, pNum = getPage()) // 추가하기
+                viewModel.fetchSearchInfo()
+            }
+        }
+    }
+
+    fun initQueryTextListener() {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    // 키워드 갱신
+                    keyword = query
+                    // 페이지 수 갱신
+                    page = 1
+
+                    viewModel.updateValue(0, keyword, getPage())
+                    loadData(loadingType = 0)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+    }
+
+    fun initScrollListener() {
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val layoutManager = recyclerView.layoutManager
+
+                if (hasMore()) {
+                    val lastSeenItem = (layoutManager as LinearLayoutManager)
+                        .findLastCompletelyVisibleItemPosition()
+                    // 현재 로드되어 있는 데이터의 마지막즈음 접근하면 다음 페이지를 추가로 불러오자
+                    if (layoutManager.itemCount <= lastSeenItem + 2) {
+                        loadData(1)
+                        checkNext(false)
+                    }
+                } else if (layoutManager != null && layoutManager.itemCount >= 1000) {
+                    Toast.makeText(this@MainActivity, "더 이상 데이터를 가져올 수 없습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        })
+    }
+
+    fun getPage(): Int = page++
+
+    fun hasMore(): Boolean {
+        return isMore
+    }
+
+    fun checkNext(flag: Boolean) {
+        isMore = flag
     }
 }
---
+++ app/src/main/java/com/task/searchrepo/MainViewModel.kt
@@ -0,0 +1,76 @@
+package com.task.searchrepo
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.task.searchrepo.model.Repo
+import com.task.searchrepo.repository.RepoService
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import kotlin.properties.Delegates
+
+class MainViewModel : ViewModel() {
+    // 검색 결과
+    var itemLiveData = MutableLiveData<MutableList<Repo>>() // 뮤터블 - 수정 가능, LiveData - 수정 불가
+    var totalItemCountLiveData = MutableLiveData<Long>()
+    var loadingLiveData = MutableLiveData<Boolean>()
+
+    private var service: RepoService
+
+    private var page by Delegates.notNull<Int>()
+    private var orderby by Delegates.notNull<Int>()
+
+    private val _keyword = MutableLiveData<String>() // 뷰모델 내부에서 설정하는 자료형
+    val keyword: LiveData<String>
+        get() = _keyword
+    private val _hasMore = MutableLiveData<Boolean>() // 다음 페이지로 넘어갈 수 있는지 확인용
+    val hasMore: LiveData<Boolean>
+        get() = _hasMore
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(RepoService.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        service = retrofit.create(RepoService::class.java)
+
+        loadingLiveData.value = false
+    }
+
+    // LiveData로 데이터 관찰
+    fun fetchSearchInfo() {
+        // 로딩 시작
+        loadingLiveData.value = true
+
+        viewModelScope.launch {
+            if (page < 35) {
+                val searchInfo = service.fetchRepo(keyword = _keyword.value!!, page = page)
+                if (searchInfo.items != null) {
+                    if (page >= 2) {
+                        itemLiveData.value?.addAll(searchInfo.items as Collection<Repo>)
+                        _hasMore.value = totalItemCountLiveData.value!! > (page * 30)
+                        // 1000개 넘으면 불러올 수 없음.
+                    } else {
+                        itemLiveData.value = (searchInfo.items as MutableList<Repo>?)!!
+                        totalItemCountLiveData.value = searchInfo.totalCount
+                    }
+                }
+            } else {
+                _hasMore.value = false
+            }
+            // 로딩 끝
+            loadingLiveData.value = false
+        }
+    }
+
+    fun updateValue(actionType: Int, input: String?, pNum: Int) {
+        when (actionType) {
+            0 -> _keyword.value = input!!
+        }
+        page = pNum
+    }
+}
